9660377ac331f0983c298e6522e41478a6e867ec
Mohamed Kamal <ku18mz@gmail.com>
2023-07-13 09:37:15 +0300

    add 101-quote that prints str using shell command

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - run echo command for print str in shell.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+system("echo \'and that piece of art is useful\" - Dora Korpar, 2015-10-19\'");
+return (1);
+}
