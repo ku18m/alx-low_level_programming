df372505b0a95fbc363f370c75c44a632121c255
Mohamed Kamal <ku18mz@gmail.com>
2023-07-13 08:40:47 +0300

    print str using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char alxstr[] = "\"Programming is like building a multilingual puzzle";
+    puts(alxstr);
+    return (0);
+}
