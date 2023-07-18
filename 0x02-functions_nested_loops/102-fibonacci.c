e499ec2d1b5c59d6ab3044a3591efa5d55d9b745
Mohamed Kamal <ku18mz@gmail.com>
2023-07-18 19:31:15 +0300

    fix 102 fibonacci 3 nums longer

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 	long int first = 1, second = 2, third, i;
 
 	printf("%lu, %lu, ", first, second);
-	for (i = 0; i < 17; i++)
+	for (i = 0; i < 16; i++)
 	{
 		third = first + second;
 		first = second + third;
 		second = first + third;
 		printf("%lu, %lu, %lu", third, first, second);
-		if (i != 16)
+		if (i != 15)
 		{
 			printf(", ");
 		}
