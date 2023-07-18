9db412833cf2be45c81ea85c674e8a0ea77cf9d8
Mohamed Kamal <ku18mz@gmail.com>
2023-07-18 19:01:57 +0300

    add 101 natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ *
+ *
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i, result = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			result = result + i;
+		}
+	}
+	printf("%d\n", result);
+return (0);
+}
