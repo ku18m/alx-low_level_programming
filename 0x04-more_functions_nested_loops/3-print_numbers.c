d1991ea276e5e6064cdb60c9c41e11c692850044
Mohamed Kamal <ku18mz@gmail.com>
2023-07-20 14:55:00 +0300

    add 4-print_most_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
- * Return: The result of multiply a and b.
+ * Return: void.
  */
 void print_numbers(void)
 {
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 & 4.
+ *
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		if (a == 50 || a == 52)
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
