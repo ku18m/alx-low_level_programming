bbb3c658ae66ab3d07f77df9e34500a2807d1794
Mohamed Kamal <ku18mz@gmail.com>
2023-07-20 18:28:41 +0300

    removing trailing spaces with some touches in 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,25 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 15 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 3 == 0)
 		{
-			printf("Buzz ");
+			printf("Fizz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
 		}
 	}
 	printf("\n");
