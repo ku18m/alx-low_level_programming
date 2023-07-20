71e8571c07974c82b8cb795a61f6eb158c03cad8
Mohamed Kamal <ku18mz@gmail.com>
2023-07-21 00:02:48 +0300

    assign the num to internal var for checker

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	int i = n;
+
+	if (i < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		i = i * -1;
 	}
-	if ((n / 10) > 0)
+	if ((i / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar((n % 10) + 48);
+	_putchar((i % 10) + 48);
 }
