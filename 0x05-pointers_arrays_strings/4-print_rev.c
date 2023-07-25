c7a3858ccc42e868c35d5e049984b0ec70c4e624
Mohamed Kamal <ku18mz@gmail.com>
2023-07-25 04:11:49 +0300

    update 4 checker test

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,12 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-	while (i >= 0)
-	{
-		_putchar(*s);
-		i--;
-		s--;
-	}
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+
+	for (--i; i >= 0; --i)
+		_putchar(s[i]);
 	_putchar('\n');
 }
