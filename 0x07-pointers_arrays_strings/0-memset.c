e3b1cc414076ac583007aa397e575e459d259567
Mohamed Kamal <ku18mz@gmail.com>
2023-07-31 20:19:28 +0300

    update 0 comment for betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * _memset - fills memory with a constant byte.
  *
- * Return: Always 0.
+ * @s: the pointer to be filled.
+ * @b: the character to fill.
+ * @n: the num of index to fill.
+ * Return: the filled pointer.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
