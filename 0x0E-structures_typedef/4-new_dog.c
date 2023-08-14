13326040b468329b252013b0299d46a680829929
Mohamed Kamal <ku18mz@gmail.com>
2023-08-15 02:08:59 +0300

    in 4 storing a copy of the elements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * _strcpy -  copies the string pointed to by src, including (\0)
+ * to the buffer pointed to by dest.
+ * @dest: a pointer to the string.
+ * @src: an array of chars.
+ *
+ * Return: (dest) coppied array.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
 
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: a pointer to the string.
+ *
+ * Return: void.
+ */
+int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
 /**
  * new_dog - creates a new dog.
  *
@@ -14,7 +50,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int i;
 
 	if (name == NULL || owner == NULL || age < 0)
 	{
@@ -26,33 +61,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	i = 0;
-	while (name[i] != '\0')
-	{
-		i++;
-	}
-	new_dog->name = malloc(i + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	i = 0;
-	while (owner[i] != '\0')
-	{
-		i++;
-	}
-	new_dog->owner = malloc(i + 1);
-	if (new_dog->owner == NULL)
+	new_dog->name = malloc((_strlen(name)) + 1);
+	new_dog->owner = malloc((_strlen(owner)) + 1);
+	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->name = name;
-	new_dog->owner = owner;
 	return (new_dog);
 }
