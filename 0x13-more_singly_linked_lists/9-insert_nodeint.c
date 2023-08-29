2990cd919af7e625f536414a8defe8337183e5fe
Mohamed Kamal <ku18mz@gmail.com>
2023-08-29 23:42:44 +0300

    refactor 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,26 +14,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp, *new;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	tmp = *head;
-	if (idx != 0)
-		idx--;
 	i = 0;
-	while (tmp && (i < idx))
+	while (tmp != NULL && i < idx - 1)
 	{
 		tmp = tmp->next;
 		i++;
 	}
-	if ((idx == i) || (tmp != NULL && idx == 0))
+	if (tmp == NULL)
 	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		if (i != 0)
-			new->next = tmp->next, tmp->next = new;
-		else
-			*head = new, new->next = NULL;
-		return (new);
+		free(new);
+		return (NULL);
 	}
-	return (NULL);
+	new->next = tmp->next;
+	tmp->next = new;
+	return (new);
 }
