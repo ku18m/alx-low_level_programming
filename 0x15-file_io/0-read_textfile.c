e683b47e88e927c6a0f0a62892cdaa0bb202a3b8
Mohamed Kamal <ku18mz@gmail.com>
2023-09-04 20:57:52 +0300

    replace ints with typedefs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,14 +17,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, 0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters));
 	if (!buffer)
 		return (0);
 	chars = read(fd, buffer, letters);
-	count = write(1, buffer, chars);
+	count = write(STDOUT_FILENO, buffer, chars);
 	close(fd);
 	free(buffer);
 	return (count);
