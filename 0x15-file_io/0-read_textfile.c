eba673b4d29f24fd7b98f332c22281faa7e0a188
Mohamed Kamal <ku18mz@gmail.com>
2023-09-04 20:52:33 +0300

    in 0 remove wrong letters input handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t count, chars;
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
 	fd = open(filename, 0);
 	if (fd == -1)
