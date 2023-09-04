3b488d349e677984da31cbd34ed57c5371d48fb1
Mohamed Kamal <ku18mz@gmail.com>
2023-09-04 21:00:21 +0300

    in 0 back to typedefs for check 7

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,15 +17,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, 00);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters));
 	if (!buffer)
 		return (0);
 	chars = read(fd, buffer, letters);
-	count = write(1, buffer, chars);
-	close(fd);
+	count = write(STDOUT_FILENO, buffer, chars);
 	free(buffer);
+	close(fd);
 	return (count);
 }
