9cd68fc5d794cee594b6bd4255feb15c6f3bf1d8
Mohamed Kamal <ku18mz@gmail.com>
2023-10-13 01:39:33 +0300

    assign prev value (new to doubly)

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 		*head = new_node, new_node->next = NULL;
 	else
-		new_node->next = *head, *head = new_node;
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 
 	return (new_node);
 }
