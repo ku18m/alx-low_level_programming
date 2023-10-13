d9231b02e64feda34f23b1d44ec4c122acaa613c
Mohamed Kamal <ku18mz@gmail.com>
2023-10-13 04:08:03 +0300

    refactor the code for check 8 & 9

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,31 +1,6 @@
 #include "lists.h"
 
 /**
- * _add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- *
- * @head: pointer to a node.
- * @n: The value to add.
- *
- * Return: the address of the new element, or NULL if it failed.
- */
-dlistint_t *_add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new_node;
-
-	new_node = malloc(sizeof(dlistint_t *));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->next = (*head)->prev->next;
-	new_node->prev = (*head)->prev;
-	new_node->n = n;
-	(*head)->prev->next = new_node;
-	(*head)->prev = new_node;
-
-	return (new_node);
-}
-
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  *
  * @h: pointer to a node.
@@ -36,34 +11,43 @@ dlistint_t *_add_dnodeint(dlistint_t **head, const int n)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node, *head;
 	unsigned int i;
-	dlistint_t *new_node = NULL, *head = *h;
 
-	if (head == NULL && idx != 0)
-		return (new_node);
-
+	new_node = NULL;
 	if (idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
 		return (new_node);
 	}
 
-	while (head->prev)
-		head = head->prev;
-
-	i = 0;
-	while (head && i <= idx)
+	head = *h, i = 1;
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+	while (head != NULL)
 	{
 		if (i == idx)
 		{
-			new_node = _add_dnodeint(&head->prev->next, n);
-			return (new_node);
+			if (head->next == NULL)
+				new_node = add_dnodeint_end(h, n);
+			else
+			{
+				new_node = malloc(sizeof(dlistint_t));
+				if (new_node != NULL)
+				{
+					new_node->n = n;
+					new_node->next = head->next;
+					new_node->prev = head;
+					head->next->prev = new_node;
+					head->next = new_node;
+				}
+			}
+			break;
 		}
-		head = head->next, i++;
+		head = head->next;
+		i++;
 	}
-
-	if (i == idx)
-		new_node = add_dnodeint_end(h, n);
 
 	return (new_node);
 }
