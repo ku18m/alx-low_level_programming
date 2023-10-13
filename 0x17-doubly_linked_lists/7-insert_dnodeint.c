0674b57581d8453b50cf101a2b2204f92c487d76
Mohamed Kamal <ku18mz@gmail.com>
2023-10-13 03:37:00 +0300

    create a new fun to handle prev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * _add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ *
+ * @head: pointer to a node.
+ * @n: The value to add.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *_add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t *));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->next = (*head)->prev->next;
+	(*head)->prev->next = new_node;
+	new_node->prev = (*head)->prev;
+	(*head)->prev = new_node;
+	new_node->n = n;
+
+	return (new_node);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  *
  * @h: pointer to a node.
@@ -14,8 +39,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *new_node = NULL, *head = *h;
 
-	if (head == NULL)
-		return (NULL);
+	if (head == NULL || idx == 0)
+	{
+		new_node = add_dnodeint(h, n);
+		return (new_node);
+	}
 
 	while (head->prev)
 		head = head->prev;
@@ -25,13 +53,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
-			new_node = add_dnodeint(&head->prev->next, n);
-			break;
+			new_node = _add_dnodeint(&head->prev->next, n);
+			return (new_node);
 		}
 		head = head->next, i++;
 	}
 
-	if (i == idx && new_node == NULL)
+	if (i == idx)
 		new_node = add_dnodeint_end(h, n);
 
 	return (new_node);
