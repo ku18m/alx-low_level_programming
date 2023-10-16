776ce6dfe5328b3f59b3d9bca3a2825e14b06c7f
Mohamed Kamal <ku18mz@gmail.com>
2023-10-16 16:01:14 +0300

    add \n to the script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ku18m/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgm.so
+wget -P /tmp/ https://github.com/ku18m/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgm.so
 export LD_PRELOAD =/tmp/libgm.so
