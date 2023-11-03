c4e2bc6e15099a06f265c4cf262b9c1fae739198
Mohamed Kamal <ku18mz@gmail.com>
2023-11-03 17:37:14 +0200

    add task 5 function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -29,7 +29,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
+void hash_table_print(const hash_table_t *ht);
 /*Functions*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
