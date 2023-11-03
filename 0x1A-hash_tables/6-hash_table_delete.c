fb08cb793724b06cf5d97cf14b89c816f08c9543
Mohamed Kamal <ku18mz@gmail.com>
2023-11-03 18:08:52 +0200

    modify loop condition

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -15,18 +15,17 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp, *tmp_next;
 	unsigned long int i;
 
-	if (!ht || !ht->array || ht->size == 0)
+	if (!ht)
 		return;
 
 	i = 0;
 	while (i < ht->size)
 	{
-		tmp = ht->array[i];
-		while (tmp)
+		tmp_next = ht->array[i];
+		while ((tmp = tmp_next))
 		{
-			tmp_next = tmp->next;
-			free(tmp->key), free(tmp->next), free(tmp->value), free(tmp);
-			tmp = tmp_next;
+			tmp_next = tmp_next->next;
+			free(tmp->key), free(tmp->value), free(tmp);
 		}
 		i++;
 	}
