8a647f99028104ca0a67825d0e299970603187f5
Mohamed Kamal <ku18mz@gmail.com>
2024-02-08 04:48:06 +0200

    add found in the middle, makes O(1) print nothing

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,13 +29,10 @@ int jump_search(int *array, size_t size, int value)
 		if (array[start] == value)
 			return (start);
 		if (array[end] >= value || end >= size)
-		{
-			printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 			break;
-		}
 		start += step, end += step;
 	}
-
+	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 	while (start <= end && start < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
