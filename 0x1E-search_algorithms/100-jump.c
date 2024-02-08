831cd268d81b2c50ca34a148d206bda3d144da67
Mohamed Kamal <ku18mz@gmail.com>
2024-02-08 04:52:28 +0200

    change print logic

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 		start += step, end += step;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-	while (start <= end && start < size)
+	while (start < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
 		if (array[start] == value)
